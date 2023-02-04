a47d67f692813ab354b4b099dd54e768ab642f01
AndrewPerepelkin <avp.tep@rambler.ru>
2023-02-04 06:38:34 +0300

    feat: added route for user updating

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const auth = require('../middleware/auth.middleware');
 const User = require('../models/User');
-const router = express.Router({ mergeParams: true })
+const router = express.Router({mergeParams: true});
 
 router.get('/', async (req, res) => {
-    try {
+  try {
     const list = await User.find();
     res.status(200).send(list);
   } catch (error) {
@@ -11,8 +12,25 @@ router.get('/', async (req, res) => {
       message: 'На сервере произошла ошибка. Попробуйте позже...'
     });
   }
-})
+});
 
-router.patch('/:id', async (req, res) => {})
+router.patch('/:userId', auth, async (req, res) => {
+  try {
+    const {userId} = req.params;
 
-module.exports = router
+    if (userId === req.user._id) {
+      const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+        new: true
+      });
+      res.send(updatedUser);
+    } else {
+      res.status(401).json({message: 'Unauthorized'});
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже...'
+    });
+  }
+});
+
+module.exports = router;
